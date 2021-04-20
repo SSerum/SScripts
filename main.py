c070282c365ebba253bb243565e67963ebfae866
Serum <82846098+SSerum@users.noreply.github.com>
2021-04-20 08:39:09 -0400

    Random Python File

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+Person = 0
+Person = random.randint(1,11)
+print(Person)
+if Person == 1:
+  print("1")
+if Person == 2:
+  print("2")
+if Person == 3:
+  print("3")
+if Person == 4:
+  print("4")
+if Person == 5:
+  print("5")
+if Person == 6:
+  print("6")
+if Person == 7:
+  print("7")
+if Person == 8:
+  print("8")
+if Person == 9:
+  print("9")
+if Person == 10:
+  print("10")
+if Person == 11:
+  print("11")
